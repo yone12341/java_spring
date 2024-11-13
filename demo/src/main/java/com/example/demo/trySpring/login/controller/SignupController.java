f7d9295d6b41e30aef22e20848e18d995e09fdf6
yone <yone@yone-himajin.com>
2024-11-13 23:01:06 +0900

    とりあえず

---
+++ demo/src/main/java/com/example/demo/trySpring/login/controller/SignupController.java
@@ -0,0 +1,42 @@
+package com.example.demo.trySpring.login.controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class SignupController {
+
+    private Map<String, String> radioMarriage;
+
+    private Map<String, String> initRadioMarriage() {
+
+        Map<String, String> radio = new LinkedHashMap<>();
+
+        radio.put("既婚","true");
+        radio.put("未婚","true");
+
+        return radio;
+    }
+
+    @GetMapping("/signup")
+    public String getSignUp(Model model) {
+
+        radioMarriage = initRadioMarriage();
+
+        model.addAttribute("radioMarriage", radioMarriage);
+
+        return "login/signup";
+    }
+
+    @PostMapping("/signup")
+    public String postSingUp(Model model) {
+
+        return "redirect/login";
+    }
+
+}

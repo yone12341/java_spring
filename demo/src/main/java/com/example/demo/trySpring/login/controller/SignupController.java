d45102ee7ef7598934bb5caa6d9ca8e7311e7730
yone <yone@yone-himajin.com>
2024-11-14 22:53:26 +0900

    バリデーション途中

--- demo/src/main/java/com/example/demo/trySpring/login/controller/SignupController.java
+++ demo/src/main/java/com/example/demo/trySpring/login/controller/SignupController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,7 +39,7 @@ public class SignupController {
     }
 
     @PostMapping("/signup")
-    public String postSingUp(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
+    public String postSingUp(@ModelAttribute @Validated SignupForm form, BindingResult bindingResult, Model model) {
 
         if(bindingResult.hasErrors()) {
             return getSignUp(form, model);
--- demo/src/main/java/com/example/demo/trySpring/login/domain/model/SignupForm.java
+++ demo/src/main/java/com/example/demo/trySpring/login/domain/model/SignupForm.java
@@ -1,21 +1,42 @@
 package com.example.demo.trySpring.login.domain.model;
 
-import java.sql.Date;
+import java.util.Date;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.validation.constraints.AssertFalse;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class SignupForm {
     
+    @NotBlank
+    @Email
     private String userId;
+
+    @NotBlank
+    @Length(min=4,max=100)
+    @Pattern(regexp = "^[a-zA-Z0-9$]")
     private String passWord;
+
+    @NotBlank
     private String userName;
 
+    @NotNull
     @DateTimeFormat(pattern="yyyy/MM/dd")
     private Date birthday;
 
+    @Min(20)
+    @Max(100)
     private int age;
+
+    @AssertFalse
     private boolean marriage;
 }
